15c2e765a0acae9c6e9ab73570a57e871a312e6b
Shedriq <jerrydwain4@gmail.com>
2023-04-28 13:18:53 +0000

    tasks

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+""" Duck typing - first element of a sequence """
+from typing import Any, Union, Sequence, Iterable, List, Tuple
 
-safe_first_element = __import__('100-safe_first_element').safe_first_element
 
-print(safe_first_element.__annotations__)
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Safe first element """
+    if lst:
+        return lst[0]
+    else:
+        return None
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
+""" Type Checking  """
+from typing import Tuple, List
 
-zoom_array = __import__('102-type_checking').zoom_array
 
-print(zoom_array.__annotations__)
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Zoom Array """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
